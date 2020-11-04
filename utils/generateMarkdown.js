1b393b71cc32fc4d0e153a902dac5f0ebd70468e
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-04 10:09:12 -0500

    made change to README Format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,9 @@
 
 // function to generate markdown for README
 function generateMarkdown(userResponse, userInfo) {
-
-  var result = (`
-  # ${userResponse.title} 
+  var licenseBadge = generateBadge(userResponse.license)
+  var result = (`# ${userResponse.title}
+![License](${licenseBadge} \n
 ${userResponse.description}
 ## Table of Contents
 \n* [Installation](#Installation)
@@ -30,6 +30,39 @@ ${userResponse.tests}
 
 
   return result ;
+}
+
+function generateBadge(license) {
+  let badge;
+  
+  switch (license) {
+    case "GNU AGPLv3":
+      badge = { name: "GNU+AGPLv3", color: "orange" };
+      break;
+    case "GNU GPLv3":
+      badge = { name: "GNU+GPLv3", color: "red" };
+      break;
+    case "GNU LGPLv3":
+      badge = { name: "GNU+LGPLv3", color: "blue" };
+      break;
+    case "Mozilla Public License 2.0":
+      badge = { name: "Mozilla+2.0", color: "yellow" };
+      break;
+    case "Apache License 2.0":
+      badge = { name: "Apache+2.0", color: "green" };
+      break;
+    case "MIT License":
+      badge = { name: "MIT", color: "brightgreen" };
+      break;
+    case "Boost Software License 1.0":
+      badge = { name: "Boost+Software+1.0", color: "yellowgreen" };
+      break;
+    case "The Unlicense":
+      badge = { name: "The+Unlicense", color: "blueviolet" };
+      break;
+  }
+
+  return `https://img.shields.io/static/v1?label=license&message=${badge.name}&color=${badge.color})`;
 }
 
 module.exports = generateMarkdown;
