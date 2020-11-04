4b02f0f8c55e7c8b4c69054fdfa0c2ef917babae
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-04 08:54:47 -0500

    Revert "added Markdown Formating"
    This reverts commit de9c49789045eab8b9e0729b2ac3dbb97dd817e5.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,8 @@
-
-
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return (`# ${data.title}         ![License](${license}\n\n` +
-  `## Description\n${data.projectDescription}\n\n` +
-  `## Table of Contents\n` +
-  `[Installation](#installation)\n\n` +
-  `[Usage](#usage)\n\n` +
-  `[Tests](#tests)\n\n` +
-  `[Contributing](#contributing)\n\n` +
-  `[Questions](#questions)\n\n` +
-  `[License](#license)\n\n` +
-  `## Installation\n${data.installInstructions}\n\n` +
-  `## Usage\n${data.usageInformation}\n\n` +
-  `## Tests\n${data.testInstructions}\n\n` +
-  `## Contributing\n${data.contributionGuidelines}\n\n` +
-  `## License\nThis project is licensed under ${data.license}\n\n`
-);
+  return `# ${data.title}
 
+`;
 }
 
 module.exports = generateMarkdown;
